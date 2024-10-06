f11a1abbd18a02592293625e59c5392e09ee96db
Jordan Lee <jordan.mk.lee@gmail.com>
2024-10-05 22:57:27 -0600

    Removed old PDF generation functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
 
-import { Box, Stack, Modal, AppBar, Toolbar, Typography, Button, Menu, MenuItem } from '@mui/material';
+import { Stack, AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { FileDownload } from "@mui/icons-material";
-
-import { PDFViewer } from '@react-pdf/renderer';
 
 import dayjs from "dayjs";
 
@@ -11,15 +9,12 @@ import { usePDF } from 'react-to-pdf';
 
 import Form from "./components/Form";
 import InvoicePreview from "./components/InvoicePreview";
-import GeneratedDocument from "./components/GeneratedDocument";
 
 import './App.css';
 
 
 export default function App(){
 	const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
-
-	const [pdfModalOpen, setPdfModalOpen] = useState(false);
 
 	const [companyName, setCompanyName] = useState(localStorage.getItem("companyName"));
 	const [billTo, setBillTo] = useState("");
@@ -58,29 +53,6 @@ export default function App(){
 	// TODO make a helper function to parse dollar amounts (and handle NaN errors)
 	return(
 		<Stack direction="col" style={{ height: "100vh" }}>
-			<Modal open={pdfModalOpen} onClose={() => setPdfModalOpen(false)}>
-				<Box sx={{ position: 'absolute',
-					top: "50%",
-					left: "50%",
-					transform: "translate(-50%, -50%)",
-					width: "80%",
-					height: "80%",
-					color: "white",
-					padding: "16px" }}>
-					<PDFViewer style={{ width: "100%", height: "100%" }}>
-						<GeneratedDocument
-							companyName={companyName}
-							billTo={billTo}
-							date={date}
-							invoiceNumber={invoiceNumber}
-							projectAddress={projectAddress}
-							poNumber={poNumber}
-							items={items}
-							summary={summary}/>
-					</PDFViewer>
-				</Box>
-			</Modal>
-			
 			<AppBar position="fixed" sx={{ background: "white", color: "black", boxShadow: "none" }}>
 				<Toolbar sx={{ justifyContent: "space-between" }}>
 					<Typography variant="h5">Invoice Generator</Typography>
